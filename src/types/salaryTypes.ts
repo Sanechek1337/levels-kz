15cef2ba837359d0953ed19bb0134374e87d89b3
Alexandr <rednaxela659@gmail.com>
2024-03-23 21:59:11 +0500

    feat: update salary type according api

--- src/types/salaryTypes.ts
+++ src/types/salaryTypes.ts
@@ -1,12 +1,19 @@
 import { locationType } from './locationTypes.ts';
 import { specializationType } from './specializationTypes.ts';
+import { companyType } from './companyTypes.ts';
 
 export type salaryType = {
   _id: string;
   email: string;
-  grade: string;
   location: locationType;
   specialization: specializationType;
-  yac: number;
+  company: companyType;
+  salary: {
+    base: number;
+    bonus: number;
+  };
   yoe: number;
+  yac: number;
+  grade: string;
+  created: Date;
 };
