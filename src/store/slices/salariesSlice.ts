6b84f03cd035366ba43c40d3fd99d32dd511c8ee
Alexandr <rednaxela659@gmail.com>
2024-03-23 21:53:13 +0500

    feat: create salary list slice

---
+++ src/store/slices/salariesSlice.ts
@@ -0,0 +1,31 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { salaryApi } from '../../api/salary-api.ts';
+import { salaryType } from '../../types/salaryTypes.ts';
+
+export const getSalariesList = createAsyncThunk(
+  'salariesList/getSalaries',
+  async () => {
+    return await salaryApi.getSalaries().then((res) => res.data);
+  }
+);
+
+type initialStateType = {
+  salariesList: salaryType[];
+};
+
+const initialState: initialStateType = {
+  salariesList: [],
+};
+
+export const salariesListSlice = createSlice({
+  name: 'salariesList',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getSalariesList.fulfilled, (state, action) => {
+      state.salariesList = action.payload;
+    });
+  },
+});
+
+export default salariesListSlice.reducer;
