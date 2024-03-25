1cf1ef2c593b4caf9ca6ac3491304a3ec3c97bb0
Alexandr <rednaxela659@gmail.com>
2024-03-25 19:51:48 +0500

    feat: add all salaries component to dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,15 +1,13 @@
 import { Grid } from '@mui/material';
-import { MyCompanies } from './MyCompanies.tsx';
-import { PopularCompanies } from './PopularCompanies/PopularCompanies.tsx';
 import { Salaries } from './Salaries.tsx';
 import { Traffic } from './Traffic.tsx';
-import { orders } from './data.ts';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store.ts';
+import AllSalariesList from '../AllSalariesList/AllSalariesList.tsx';
 
 export function Dashboard() {
-  const companiesList = useSelector(
-    (state: RootState) => state.companiesListReducer.companiesList
+  const { salariesList } = useSelector(
+    (state: RootState) => state.salariesListReducer
   );
 
   const cardStyles = {
@@ -24,6 +22,7 @@ export function Dashboard() {
       <Grid
         container
         spacing={3}
+        justifyContent="center"
         sx={{
           maxWidth: '1440px',
           margin: '0 auto',
@@ -39,11 +38,8 @@ export function Dashboard() {
             sx={cardStyles}
           />
         </Grid>
-        <Grid item lg={6} md={12} xs={12}>
-          <PopularCompanies companies={companiesList} sx={cardStyles} />
-        </Grid>
-        <Grid item lg={6} md={12} xs={12}>
-          <MyCompanies orders={orders} sx={cardStyles} />
+        <Grid item lg={12} md={12} xs={12}>
+          <AllSalariesList salariesList={salariesList} />
         </Grid>
       </Grid>
     </>
